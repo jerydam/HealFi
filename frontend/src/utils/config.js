9bc4a37c9cb95c9ecbd704e58b611860aedcf690
star-ica <akangbeabigael11@gmail.com>
2026-08-07 21:20:12 +0100

    mainnet

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -2,15 +2,7 @@ import { CONTRACT_ADDRESSES } from "@/lib/contract";
 
 // botchain network definitions. testnet is the default target; mainnet is here so
 // switching networks is a one-line change to ACTIVE_CHAIN_ID.
-export const BOTCHAIN_TESTNET = {
-  id: 968,
-  chainName: "botchain Testnet",
-  nativeCurrency: { name: "botchain", symbol: "botchain", decimals: 18 },
-  rpcUrls: ["https://rpc.bohr.life"],
-  blockExplorerName: "botchain testnet Explorer",
-  blockExplorerUrls: ["https://scan.bohr.life"],
-  testnet: true,
-};
+
 
 export const BOTCHAIN_MAINNET = {
   id: 677,
@@ -23,15 +15,14 @@ export const BOTCHAIN_MAINNET = {
 };
 
 export const SUPPORTED_CHAINS = {
-  [BOTCHAIN_TESTNET.id]: BOTCHAIN_TESTNET,
   [BOTCHAIN_MAINNET.id]: BOTCHAIN_MAINNET,
 };
 
 export const ACTIVE_CHAIN_ID = Number(
-  process.env.NEXT_PUBLIC_CHAIN_ID || BOTCHAIN_TESTNET.id
+  process.env.NEXT_PUBLIC_CHAIN_ID || BOTCHAIN_MAINNET.id
 );
 
-export const ACTIVE_CHAIN = SUPPORTED_CHAINS[ACTIVE_CHAIN_ID] || BOTCHAIN_TESTNET;
+export const ACTIVE_CHAIN = SUPPORTED_CHAINS[ACTIVE_CHAIN_ID] || BOTCHAIN_MAINNET;
 
 export const CONTRACTS = CONTRACT_ADDRESSES;
 
